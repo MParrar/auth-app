ddd2f9f2244f8c46cb0261a27480ec3565011974
Matias Parra <parramatias96@gmail.com>
2025-01-25 17:03:52 -0300

    feat: Use better warning message to delete user

--- frontend/src/components/RemoveUserConfirmation.jsx
+++ frontend/src/components/RemoveUserConfirmation.jsx
@@ -4,7 +4,7 @@ export const RemoveUserConfirmation = () => {
   return (
     <p className="text-white">
             Are you sure you want to delete your account? This action will
-            permanently remove your registration and close your account.
+            permanently remove your registration.
     </p>
   )
 }
